4494b09e8f2c8a87c7bce68b62e696139ab13832
Lena0912 <elenabelichenko9@gmail.com>
2024-09-06 16:50:36 +0300

    second

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,39 @@
 import { Component } from "react";
 import { QuizList } from "./QuizList/QuizList";
 import initialQuizItems from '../data.json';
+import { SearchBar } from "./SearchBar/SearchBar";
+import { QuizForm } from "./QuizForm/QuizForm";
 
 export class App extends Component {
   state = {
     quizItems: initialQuizItems,
+    topicFilter: '',
+    levelFilter: 'all',
   };
+
+  changeTopicFilter = newFilter => {
+    this.setState({
+      topicFilter: newFilter,
+    });
+  };
+
+  changeLevelFilter = newLevel => {
+    this.setState({
+      levelFilter: newLevel,
+    });
+  };
+
   render() {
-    const { quizItems } = this.state;
+    const { quizItems, topicFilter, levelFilter } = this.state;
     return (
       <div>
+        <QuizForm/>
+        <SearchBar
+          level={levelFilter}
+          topic={topicFilter}
+          onChangeTopic={this.changeTopicFilter}
+          onChangeLevel={this.changeLevelFilter}
+        />
         <QuizList items={quizItems} />
       </div>
     );
--- src/components/QuizCard/QuizCard.js
+++ src/components/QuizCard/QuizCard.js
@@ -1,11 +1,11 @@
-export const QuizCard = ({quiz}) => {
+export const QuizCard = ({quiz: {topic, level, time, questions}}) => {
     return (
       <div>
-        <h2>{quiz.topic}</h2>
+        <h2>{topic}</h2>
         <div>
-          <p>Level:{quiz.level}</p>
-          <p>Time:{quiz.time}min</p>
-          <p>Questions:{quiz.questions}</p>
+          <p>Level:{level}</p>
+          <p>Time:{time}min</p>
+          <p>Questions:{questions}</p>
         </div>
       </div>
     );
