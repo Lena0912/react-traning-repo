1c7302603e550339cfb5af250dbbef3035913d34
Lena0912 <elenabelichenko9@gmail.com>
2024-09-06 03:22:16 +0300

    first-day

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import { Component } from "react";
+import { QuizList } from "./QuizList/QuizList";
+import initialQuizItems from '../data.json';
+
+export class App extends Component {
+  state = {
+    quizItems: initialQuizItems,
+  };
+  render() {
+    const { quizItems } = this.state;
+    return (
+      <div>
+        <QuizList items={quizItems} />
+      </div>
+    );
+  }
+}
+    
+
