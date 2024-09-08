6e751380d577b731ac0daea12c29c0dc1a4fd98d
Lena0912 <elenabelichenko9@gmail.com>
2024-09-08 12:34:06 +0300

    update

--- src/components/QuizForm/QuizForm.js
+++ src/components/QuizForm/QuizForm.js
@@ -1,3 +1,45 @@
+import { Formik, Field, Form } from 'formik';
+
 export const QuizForm = () => {
-    return <div>QuizForm</div>    
-}
+  return (
+    <Formik
+      initialValues={{
+        topic: '',
+        time: '0',
+        questions: '0',
+        level: 'beginner',
+      }}
+      onSubmit={values => {
+        console.log(values);
+      }}
+    >
+      <Form>
+        <label>
+          Topic
+          <Field name="topic" />
+        </label>
+
+        <label>
+          Time
+          <Field type="number" name="time" />
+        </label>
+
+        <label>
+          Questions
+          <Field type="number" name="questions" />
+        </label>
+
+              <label>
+                  Level
+          <Field as="select" name="level">
+            <option value="beginner">Beginner</option>
+            <option value="intermediate">Intermediate</option>
+            <option value="advanced">Advanced</option>
+          </Field>
+        </label>
+
+        <button type="submit">Add quiz</button>
+      </Form>
+    </Formik>
+  );
+};
